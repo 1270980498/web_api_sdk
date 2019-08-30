0c050434f8b8739374a164155bbb132edbe1d26d
1270980498 <42734408+1270980498@users.noreply.github.com>
2019-08-30 16:11:45 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-08-30
+ * Time: 16:09
+ */
+require 'vendor/autoload.php';
+
+//hello world
+$hello = new Sky\Demo\hellor('worlddd' );
+
+echo $hello->hello();
